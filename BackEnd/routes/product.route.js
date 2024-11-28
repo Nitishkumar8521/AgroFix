00860499062e5707dbb015e321292bc748d556e0
Nitish kumar singh <nkk7367095@gmail.com>
2024-11-28 11:08:48 +0530

    adding api_key

--- BackEnd/routes/product.route.js
+++ BackEnd/routes/product.route.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 cloudinary.config({
     cloud_name: process.env.cloud_name,
-    api_key: process.env.api_key,
+    api_key: '114711923992553',
     api_secret:process.env.api_secret,
 })
 
